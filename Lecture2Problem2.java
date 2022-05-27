756cb28a5edb74fd6a3b2861126b4eb75df98d82
beibarys-jedi <102501816+beibarys-jedi@users.noreply.github.com>
2022-05-27 08:31:33 +0600

    Create Lecture2Problem2.java

---
+++ Lecture2Problem2.java
@@ -0,0 +1,25 @@
+// Rectangle Class
+class Rectangle {
+
+  // Write Fields Here
+  
+  
+  public Rectangle() {
+   
+    // Write your code here
+    
+  }
+  
+  public Rectangle(int length, int width) {
+
+    // Write your code here
+
+  }
+
+  public int getArea() {
+
+    // Write your code here
+    return 0; 
+  }
+  
+}
